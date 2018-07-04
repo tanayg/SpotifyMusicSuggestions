52da0a08d8bbbb5e8d5dc55963f956ce43977b2a
tanayg <tgottigundala@gmail.com>
2018-07-03 21:25:01 -0700

    Project started

---
+++ server.js
@@ -0,0 +1,26 @@
+// Module Dependencies
+var express = require('express')
+var stylus = require('stylus')
+var nib = require('nib')
+var logger = require('morgan')
+
+var app = express()
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.end('Hi there!')
+})
+app.listen(3000)
