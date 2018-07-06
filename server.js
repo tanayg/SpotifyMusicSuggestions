371fa4cb271d0474ef719b21f1a443e6025845ec
tanayg <tgottigundala@gmail.com>
2018-07-06 13:54:26 -0700

    Added new libraries

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var express = require('express')
 var stylus = require('stylus')
 var nib = require('nib')
 var logger = require('morgan')
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+// Spotify variables
+var client_id = '8121c945a73d47149feb3d7368cf6241'
+var client_secret = 'bbbc6f25ed394a60a774fed961cb9fc1'
+var redirect_uri = 'http://localhost:3000/home'
 
 var app = express()
 function compile(str, path) {
@@ -21,7 +29,20 @@ app.use(stylus.middleware(
 ))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function (req, res) {
-  res.end('Hello World!')
+
+app.get('/', function (req, res) { //Redirects to home page
+  //res.end('Hello World!')
+  res.redirect('/home')
 })
+
+
+app.get('/home', function(req, res, next){  //Landing page
+  res.render('home')
+})
+
+
+app.get('/login', function(req, res, next){  //Login page
+  res.end('Login Page')
+})
+
 app.listen(3000)
