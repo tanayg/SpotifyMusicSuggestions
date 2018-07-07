4d10c738da9efbd21d7a9a5a51abaea787d16b1e
tanayg <tgottigundala@gmail.com>
2018-07-06 22:55:47 -0700

    Spotify authentication now works. Still need to reformat code

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var cookieParser = require('cookie-parser');
 // Spotify variables
 var client_id = '8121c945a73d47149feb3d7368cf6241';
 var client_secret = 'bbbc6f25ed394a60a774fed961cb9fc1';
-var redirect_uri = 'http://localhost:3000/home';
+var redirect_uri = 'http://localhost:3000/top';
 var generateRandomString = function(length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -46,21 +46,109 @@ app.use(express.static(__dirname + '/public'))
   .use(cookieParser());
 
 
-app.get('/', function (req, res) { //Redirects to home page
-  //res.end('Hello World!')
-  res.redirect('/home');
-})
-
-
-app.get('/home', function(req, res, next){  //Landing page
+app.get('/', function (req, res, next) { //Before login
   res.render('home');
 })
 
 
-app.get('/login', function(req, res, next){  //Login page
+app.get('/login', function(req, res) {
   var state = generateRandomString(16);
-  res.cookie
-  res.end('Login Page');
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+app.get('/callback', function(req, res) { //requests refresh and access tokens after checking the state parameter
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', function(req, res) {// requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
+app.get('/top', function (req, res, next) { //After login
+  res.render('top');
 })
 
 app.listen(3000);
